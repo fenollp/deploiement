47593b27fbc8bd3063d441efa7c33c56940ce30d
Seb35 <seb35@seb35.fr>
2018-01-27 17:33:26 +0100

    Ma base SQLite semble un peu corrompue, peut-être par manque de place lors de sa création
    Ajout d’un peu plus de disque par défaut

--- create_gandi_server_legilibre.sh
+++ create_gandi_server_legilibre.sh
@@ -15,4 +15,4 @@ if [ -n "$sshkey" ]; then
 	arg_sshkey="--sshkey `echo $sshkey|grep -m 1 name|awk '{print $3}'`"
 fi
 
-gandi vm create --datacenter 'FR-SD5' --cores 2 --memory 4096 --ip-version 4 --size '10G' --image 'Debian 9' --run 'cd /root && wget https://raw.githubusercontent.com/Legilibre/deploiement/master/deploy_legilibre.sh && chmod +x deploy_legilibre.sh && ./deploy_legilibre.sh' --login $USER $arg_sshkey --ssh
+gandi vm create --datacenter 'FR-SD5' --cores 2 --memory 4096 --ip-version 4 --size '15G' --image 'Debian 9' --run 'cd /root && wget https://raw.githubusercontent.com/Legilibre/deploiement/master/deploy_legilibre.sh && chmod +x deploy_legilibre.sh && ./deploy_legilibre.sh' --login $USER $arg_sshkey --ssh
